4ac47d6e28e3cde67276cb03640fd8b5cab2e9ba
Anna Godoy <nabts@live.com>
2016-11-29 12:29:56 -0200

    finish signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   get '/auth/:provider/callback', to: 'users#create'
-  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :put, :patch], :as => :finish_signup
   get '/logout', to: 'users#destroy', as: 'signout'
 
 
